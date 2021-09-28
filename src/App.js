ab50f7c895e25cebc23dd5b4fbf187137999d27e
komsak007 <lert_zodiac@hotmail.com>
2021-09-28 22:14:27 +0700

    Add login admin page with context api

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { Redirect } from "react-router-dom";
+
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
@@ -11,38 +14,45 @@ import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
 
+import { AuthContext } from "./context/authContext/AuthContext";
+
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Topbar />
-        <div className="container">
-          <Sidebar />
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-        </div>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        {user ? (
+          <>
+            <Topbar />
+            <div className="container">
+              <Sidebar />
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/users">
+                <UserList />
+              </Route>
+              <Route path="/user/:userId">
+                <User />
+              </Route>
+              <Route path="/newUser">
+                <NewUser />
+              </Route>
+              <Route path="/products">
+                <ProductList />
+              </Route>
+              <Route path="/product/:productId">
+                <Product />
+              </Route>
+              <Route path="/newproduct">
+                <NewProduct />
+              </Route>
+            </div>
+          </>
+        ) : (
+          <Redirect to="/login" />
+        )}
       </Switch>
     </Router>
   );
